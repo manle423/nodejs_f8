ee0dad149901cf4e34be23a58327072987c24ce3
manle423 <trungmanle.vm@gmail.com>
2024-04-03 10:14:12 +0700

    add routes and controllers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
+const route = require('./routes')
 
 const app = express();
 const port = 3000;
 
 // Sử dụng middleware để phục vụ các tệp tĩnh từ thư mục 'public'
-app.use(express.static(path.join( __dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan("combined"));
@@ -22,14 +31,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-//route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// routes init
+route(app)
 
 //port
 app.listen(port, () => {
